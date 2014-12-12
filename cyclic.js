cd26ed7a0eea81749b6368588c38ab0428c8be13
Benjamin Lupton <b@lupton.cc>
2014-12-12 12:08:39 +1100

    Fix https://github.com/bevry/safefs/issues/5

--- cyclic.js
+++ cyclic.js
@@ -1,13 +1,19 @@
-// v1.3.7 November 1, 2013
+// December 12, 2014
 // https://github.com/bevry/base
 (function(){
 	var fsUtil = require('fs'),
+		pathUtil = require('path'),
+		existsSync = fsUtil.existsSync || pathUtil.existsSync /* node 0.6 compat */,
 		name = require('./package.json').name;
-	if ( fsUtil.existsSync('.git') === true && fsUtil.existsSync('./node_modules/'+name) === false ) {
+	if ( existsSync('.git') === true && existsSync('./node_modules/'+name) === false ) {
 		require('child_process').spawn(
 			process.platform.indexOf('win') === 0 ? process.execPath.replace('node.exe', 'npm.cmd') : 'npm',
 			['install', '--force', name],
-			{env:process.env, cwd:process.cwd(), stdio:'inherit'}
+			{
+				env: process.env,
+				cwd: process.cwd(),
+				stdio:'inherit' /* stdio doesn't exist below node <0.8 but oh well */
+			}
 		).on('error', console.log).on('close', console.log);
 	}
 })()
