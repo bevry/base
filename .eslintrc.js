68cbd95f76eedfc183e59db1c3d8f63728a9a8aa
Benjamin Lupton <balupton@users.noreply.github.com>
2015-11-30 15:42:12 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
-// 20 September 2015
+// 2015 November 30
 // https://github.com/bevry/base
 // http://eslint.org
-{
+module.exports = {
 	"parser": "babel-eslint",
 	"ecmaFeatures": {
 		"arrowFunctions": true,
@@ -32,7 +32,9 @@
 	"env": {
 		"browser": true,
 		"node": true,
-		"es6": true
+		"es6": true,
+		"commonjs": true,
+		"amd": true
 	},
 	"rules": {
 		// ----------------------------
@@ -59,6 +61,7 @@
 
 		// --------------------------------------
 		// Possible Errors
+		// The following rules point out areas where you might have made mistakes.
 
 		// ES6 supports dangling commas
 		"comma-dangle": 0,
@@ -87,11 +90,11 @@
 		// Disallow duplicate case statements in a switch
 		"no-duplicate-case": 2,
 
-		// Allow empty block statements, they are useful for clarity
-		"no-empty": 0,
-
 		// Disallow empty [] in regular expressions as they cause unexpected behaviour
 		"no-empty-character-class": 2,
+
+		// Allow empty block statements, they are useful for clarity
+		"no-empty": 0,
 
 		// Overwriting the exception argument in a catch statement can cause memory leaks in some browsers
 		"no-ex-assign": 2,
@@ -128,11 +131,10 @@
 		"no-regex-spaces": 2,
 
 		// Seems like a good idea to error about this
-		// "no-reserved-keys": 2,
-		"quote-props": ["consistent-as-needed", 2],
+		"no-sparse-arrays": 2,
 
 		// Seems like a good idea to error about this
-		"no-sparse-arrays": 2,
+		"no-unexpected-multiline": 2,
 
 		// Seems like a good idea to error about this
 		"no-unreachable": 2,
@@ -149,6 +151,10 @@
 
 		// --------------------------------------
 		// Best Practices
+		// These are rules designed to prevent you from making mistakes. They either prescribe a better way of doing something or help you avoid footguns.
+
+		// Meh
+		"accessor-pairs": 0,
 
 		// This rule seems buggy
 		"block-scoped-var": 0,
@@ -164,6 +170,12 @@
 
 		// If we don't have a default cause, it probably means we should throw an error
 		"default-case": 2,
+
+		// Dots should be on the newlines
+		// chainableThingy
+		//   .doSomething()
+		//   .doSomethingElse()
+		"dot-location": [2, "property"],
 
 		// Use dot notation where possible
 		"dot-notation": 2,
@@ -182,6 +194,9 @@
 		// They are very slow
 		"no-caller": 2,
 
+		// Wow...
+		"no-case-declarations": 2,
+
 		// Seems like a good idea to error about this
 		"no-div-regex": 2,
 
@@ -190,6 +205,9 @@
 
 		// Seems like a good idea to error about this
 		"no-empty-label": 2,
+
+		// Seems sensible
+		"no-empty-pattern": 2,
 
 		// We know that == null is a null and undefined check
 		"no-eq-null": 0,
@@ -209,8 +227,14 @@
 		// Use zero when doing decimals, otherwise it is confusing
 		"no-floating-decimal": 2,
 
+		// Cleverness is unclear
+		"no-implicit-coercion": 2,
+
 		// A sneaky way to do evals
 		"no-implied-eval": 2,
+
+		// We sometimes use this for binds, so warn but not error
+		"no-invalid-this": 1,
 
 		// Use proper iterators instead
 		"no-iterator": 2,
@@ -223,6 +247,9 @@
 
 		// Loop functions always cause problems, as the scope isn't clear through iterations
 		"no-loop-func": 2,
+
+		// This is a great idea
+		"no-magic-numbers": 2,
 
 		// We like multi spaces for clarity
 		// E.g. We like
@@ -239,19 +266,22 @@
 		"no-native-reassign": 2,
 
 		// We never use this, it seems silly to allow this
-		"no-new": 2,
-
-		// We never use this, it seems silly to allow this
 		"no-new-func": 2,
 
 		// We never use this, it seems silly to allow this
 		"no-new-wrappers": 2,
 
 		// We never use this, it seems silly to allow this
-		"no-octal": 2,
+		"no-new": 2,
 
 		// We never use this, it seems silly to allow this
 		"no-octal-escape": 2,
+
+		// We never use this, it seems silly to allow this
+		"no-octal": 2,
+
+		// Warn for awareness but don't enforce
+		"no-param-reassign": 1,
 
 		// We use process.env wisely
 		"no-process-env": 0,
@@ -280,6 +310,12 @@
 		// We never use this, it seems silly to allow this
 		"no-unused-expressions": 2,
 
+		// Seems sensible
+		"no-useless-call": 2,
+
+		// Seems sensible
+		"no-useless-concat": 2,
+
 		// We never use this, it seems silly to allow this
 		"no-void": 2,
 
@@ -287,7 +323,7 @@
 		"no-warning-comments": [1, { "terms": ["todo", "fixme"], "location": "anywhere" }],
 
 		// We never use this, it seems silly to allow this
-		// "no-with": true,
+		"no-with": 2,
 
 		// Always specify a radix to avoid errors
 		"radix": 2,
@@ -305,6 +341,7 @@
 
 		// --------------------------------------
 		// Strict Mode
+		// These rules relate to using strict mode.
 
 		// Force strict mode in the global scope as we want it everywhere
 		"strict": 0,
@@ -313,10 +350,14 @@
 
 		// --------------------------------------
 		// Variables
+		// These rules have to do with variable declarations.
+
+		// We don't care
+		"init-declaration": 0,
 
 		// Don't allow the catch method to shadow objects as browsers handle this differently
 		// Update: We don't care for IE8
-		// "no-catch-shadow": 2,
+		"no-catch-shadow": 0,
 
 		// Don't use delete, it disables optimisations
 		"no-delete-var": 2,
@@ -324,17 +365,17 @@
 		// We never use this, it seems silly to allow this
 		"no-label-var": 2,
 
-		// We use shadowing
-		"no-shadow": 0,
-
 		// We never use this, it seems silly to allow this
 		"no-shadow-restricted-names": 2,
 
-		// Error when an undefined variable is used
-		"no-undef": 2,
+		// We use shadowing
+		"no-shadow": 0,
 
 		// Makes sense
 		"no-undef-init": 2,
+
+		// Error when an undefined variable is used
+		"no-undef": 2,
 
 		// typeof blah === 'undefined' should always be used
 		"no-undefined": 2,
@@ -347,7 +388,14 @@
 
 
 		// --------------------------------------
-		// Node.js
+		// Node.js and CommonJS
+		// These rules are specific to JavaScript running on Node.js or using CommonJS in the browser.
+
+		// Seems to difficult to enforce
+		"callback-return": 0,
+
+		// We use require where it is appropriate to use it
+		"global-require": 0,
 
 		// Force handling of callback errors
 		"handle-callback-err": 2,
@@ -372,22 +420,14 @@
 
 
 		// --------------------------------------
-		// ES6
-
-		// @TODO This probably should be an error
-		// however it is useful for: for ( var key in obj ) {
-		// which hopefully is more performant than let (@TODO check if it actually is more performant)
-		"no-var": 1,
-
-		// Seems the most consistent location for it
-		"generator-star-spacing": [2, "before"],
-
-
-		// --------------------------------------
 		// Stylistic
+		// These rules are purely matters of style and are quite subjective.
 
-		// Use tabs and indent case blocks
-		"indent": [2, "tab", {"SwitchCase": 1}],
+		// We don't use spaces with brackets
+		"array-bracket-spacing": [2, "never"],
+
+		// Disallow or enforce spaces inside of single line blocks
+		"block-spacing": [2, "always"],
 
 		// Opening brace on same line, closing brace on its own line, except when statement is a single line
 		"brace-style": [2, "stroustrup", { "allowSingleLine": true }],
@@ -401,6 +441,9 @@
 		// Commas go last, we have tooling to detect if we forget a comma
 		"comma-style": [2, "last"],
 
+		// Require or disallow padding inside computed properties
+		"computed-property-spacing": [2, "never"],
+
 		// Enabling this was incredibly annoying when doing layers of nesting
 		"consistent-this": 0,
 
@@ -413,14 +456,44 @@
 		// Prefer to define functions via variables
 		"func-style": [1, "declaration"],
 
+		// Sometimes short names are appropriate
+		"id-length": 0,
+
+		// Camel case handles this for us
+		"id-match": 0,
+
+		// Use tabs and indent case blocks
+		"indent": [2, "tab", {"SwitchCase": 1}],
+
+		// Prefer double qoutes for JSX properties: <a b="c" />, <a b='"' />
+		"jsx-quotes": [2, "prefer-double"],
+
 		// Space after the colon
 		"key-spacing": [2, {
 			"beforeColon": false,
 			"afterColon": true
 		}],
 
+		// Enforce unix line breaks
+		"linebreak-style": [2, "unix"],
+
+		// Enforce new lines before block comments
+		"lines-around-comment": [2, { "beforeBlockComment": true, "allowBlockStart": true } ],
+
+		// Disabled to ensure consistency with complexity option
+		"max-depth": 0,
+
+		// We use soft wrap
+		"max-len": 0,
+
 		// We are smart enough to know if this is bad or not
 		"max-nested-callbacks": 0,
+
+		// Sometimes we have no control over this for compat reasons, so just warn
+		"max-params": [1, 4],
+
+		// We should be able to use whatever feels right
+		"max-statements": 0,
 
 		// Constructors should be CamelCase
 		"new-cap": 2,
@@ -428,8 +501,17 @@
 		// Always use parens when instantiating a class
 		"new-parens": 2,
 
+		// We do this
+		"newline-after-var": [2, "always"],
+
 		// Don't use the array constructor when it is not needed
 		"no-array-constructor": 2,
+
+		// We never use bitwise, they are too clever
+		"no-bitwise": 2,
+
+		// We use continue
+		"no-continue": 0,
 
 		// We like inline comments
 		"no-inline-comments": 0,
@@ -444,21 +526,25 @@
 		// We use multiple empty lines for styling
 		"no-multiple-empty-lines": 0,
 
+		// Sometimes it is more understandable with a negated condition
+		"no-negated-condition": 0,
+
 		// Sometimes these are useful
 		"no-nested-ternary": 0,
 
 		// Use {} instead of new Object()
 		"no-new-object": 2,
 
-		// We never use this, it seems silly to allow this
-		// "no-space-before-semi": 2,
-		"semi-spacing": [2, {"before": false, "after": true}],
+		// We use plus plus
+		"no-plusplus": 0,
+
+		// Handled by other rules
+		"no-restricted-syntax": 0,
 
 		// We never use this, it seems silly to allow this
 		"no-spaced-func": 2,
 
 		// Sometimes ternaries are useful
-		// @TODO revise
 		"no-ternary": 0,
 
 		// Disallow trailing spaces
@@ -467,30 +553,39 @@
 		// Sometimes this is useful when avoiding shadowing
 		"no-underscore-dangle": 0,
 
-		// Unless it is needed such as IIFE rule then warn as sometimes it can be useful for clarity
-		// "no-wrap-func": 2,
-		"no-extra-parens": 1,
+		// Sensible
+		"no-unneeded-ternary": 2,
+
+		// We want to do this
+		"object-curly-spacing": [2, "always"],
 
 		// We like multiple var statements
 		"one-var": 0,
 
-		// This rule doesn't appear to work correclty
-		"padded-blocks": 0,
-
 		// Force use of shorthands when available
 		"operator-assignment": [2, "always"],
 
-		// Quote object property keys only when needed
-		"quote-props": [2, "as-needed"],
+		// Should be before, but not with =, *=, /=, += lines
+		// @TODO figure out how to enforce
+		"operator-linebreak": 0,
+
+		// This rule doesn't appear to work correclty
+		"padded-blocks": 0,
+
+		// Seems like a good idea to error about this
+		"quote-props": [2, "consistent-as-needed"],
 
 		// Use single quotes where escaping isn't needed
 		"quotes": [2, "single", "avoid-escape"],
 
-		// Never use semicolons
-		"semi": [2, "never"],
+		// We use YUIdoc
+		"require-jsdoc": 0,
 
 		// If semi's are used, then add spacing after
 		"semi-spacing": [2, {"before": false, "after": true}],
+
+		// Never use semicolons
+		"semi": [2, "never"],
 
 		// We don't care if our vars are alphabetical
 		"sort-vars": 0,
@@ -502,8 +597,10 @@
 		"space-before-blocks": [2, "always"],
 
 		// function () {, get blah () {
-		// "space-before-function-parentheses": [2, "always"],
 		"space-before-function-paren": [2, "always"],
+
+		// We do this
+		"space-before-keywords": [2, "always"],
 
 		// This is for spacing between [], so [ 1, 2, 3 ] which we don't want
 		"space-in-brackets": 0,
@@ -527,6 +624,67 @@
 
 		// We use this
 		// @TODO revise this
-		"wrap-regex": 2
+		"wrap-regex": 2,
+
+
+		// --------------------------------------
+		// ECMAScript 6
+
+		// Sensible to create more informed and clear code
+		"arrow-body-style": [2, "as-needed"],
+
+		// We do this, no reason why, just what we do
+		"arrow-parens": [2, "always"],
+
+		// Require consistent spacing for arrow functions
+		"arrow-spacing": 2,
+
+		// Makes sense as otherwise runtime error will occur
+		"constructor-super": 2,
+
+		// Seems the most consistent location for it
+		"generator-star-spacing": [2, "before"],
+
+		// Seems sensible
+		"no-arrow-condition": 2,
+
+		// Seems sensible
+		"no-class-assign": 2,
+
+		// Makes sense as otherwise runtime error will occur
+		"no-const-assign": 2,
+
+		// Makes sense as otherwise runtime error will occur
+		"no-dupe-class-members": 2,
+
+		// Makes sense as otherwise runtime error will occur
+		"no-this-before-super": 2,
+
+		// @TODO This probably should be an error
+		// however it is useful for: for ( var key in obj ) {
+		// which hopefully is more performant than let (@TODO check if it actually is more performant)
+		"no-var": 1,
+
+		// Enforce ES6 object shorthand
+		"object-shorthand": 2,
+
+		// Sure, why not
+		"prefer-arrow-callback": 2,
+
+		// Sure, why not
+		"prefer-const": 1,
+
+		// Controversial change, but makes sense to move towards to reduce the risk of bad people overwriting apply and call
+		// https://github.com/eslint/eslint/issues/2939
+		"prefer-reflect": 1,
+
+		// Sure, why not
+		"prefer-spread": 2,
+
+		// Makes sense to move towards
+		"prefer-template": 1,
+
+		// Makes sense
+		"require-yield": 2
 	}
 }
